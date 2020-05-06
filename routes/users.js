e122bdc3c844c67fa4e655aef72aa35bd64a47f0
Walter <walter@medialand.tw>
2020-05-07 01:58:23 +0800

    init

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+router.all('/', function(req, res, next) {
+  next()
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a get');
+});
+
+router.post('/', function(req, res, next) {
+  res.send('respond with a post');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('respond with a put');
+});
+
+router.delete('/', function(req, res, next) {
+  res.send('respond with a delete');
+});
+
+module.exports = router;
