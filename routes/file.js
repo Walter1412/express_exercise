402afb1afa9f8f241b817a859d31d473efdc1b10
Walter <walter@medialand.tw>
2020-05-08 02:01:39 +0800

    test file reader

--- routes/file.js
+++ routes/file.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.dir(req.query)
-  res.redirect('/')
+  // console.dir(req.query)
+  // res.redirect('/')
+  fs.readFile(path.join(__dirname, './db/product.json'),'utf-8',function (err,data){
+    if(err){
+      console.log(err);
+    }else{
+      console.log('data', data)
+    }
+  })
 });
 
 router.post('/', function(req, res, next) {
