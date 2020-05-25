2d745e23709a2f4b986c2c1530ca5645fb008966
CI-Walter <walter.huang@cloud-interactive.com>
2020-05-25 18:52:18 +0800

    design mongodb

---
+++ config/db.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+var chalk = require('chalk')
+var url = 'mongodb+srv://walter:12345@cluster0-ibsha.mongodb.net/MyTest'
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log(chalk.bgGreenBright('MongoDB Connected')))
+  .catch(err => console.log(chalk.red(err)))
+
+module.exports = mongoose
--- routes/login.js
+++ routes/login.js
@@ -3,42 +3,10 @@ var express = require('express')
 var router = express.Router()
 var multer = require('multer')
 var upload = multer()
-var mongoose = require('mongoose')
+var { cloneDeep, isNull } = require('lodash')
 
 // Model
-var userModel = require('../models/users')
-
-var Schema = mongoose.Schema
-var url = 'mongodb+srv://walter:12345@cluster0-ibsha.mongodb.net/MyTest'
-
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-var testSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true
-    },
-    content: String,
-    author: String
-  },
-  { collection: 'test' }
-)
-// console.log('testSchema :>> ', testSchema);
-// console.log('userModel :>> ', userModel);
-var item = {
-  title:'test',
-  content:'trest',
-  author:'test'
-}
-
-var UserData = mongoose.model('UserData', testSchema)
-
-const data = new UserData(item)
-data.save()
+var userModel = require('../models/user')
 
 router.get('/', function(req, res, next) {
   UserData.find().then(function(doc) {
@@ -48,7 +16,22 @@ router.get('/', function(req, res, next) {
 
 router.post('/', upload.array(), function(req, res, next) {
   const { body, query } = req
-  const { title, content, author } = req
+  const item = cloneDeep(body)
+  const userData = new userModel(item)
+
+  userModel.findOne({ email: item.email }, 'name').exec(function(err, user) {
+    console.log('user :>> ', user)
+  })
+
+  // userData.save()
+
+  res.send('OK')
+})
+
+router.delete('/', upload.array(), function(req, res, next) {
+  const { body, query } = req
+  console.log('body :>> ', body)
+  console.log('query :>> ', query)
 
   res.send('OK')
 })
