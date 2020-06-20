a056fbf07d7605484c48846ceeb48013605ac15e
CI-Walter <walter.huang@cloud-interactive.com>
2020-06-20 17:09:29 +0800

    create token test

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,4 @@
 var express = require('express')
-var jwt = require('express-jwt')
 var router = express.Router()
 import Login from './classes/login'
 // Model
@@ -7,28 +6,24 @@ var UserModel = require('../models/user')
 
 const login = new Login(UserModel)
 
-const test = jwt.sign(user, app.get('secret'), {
-  expiresIn: 120
-})
-
-console.log('test :>> ', test)
-/* POST */
-router.post(
-  '/',
-  jwt({ secret: 'shhhhhhared-secret' }),
-  async (req, res, next) => {
-    const { body } = req
-    const { user, password } = body
-    const filter = {
-      email: user,
-      password
-    }
-    try {
-      const existUser = await login.getItem(filter)
-      console.log('existUser', existUser)
-      res.send({})
-    } catch (error) {}
+/* POST Json format */
+router.post('/', async (req, res, next) => {
+  const { body } = req
+  const { user, password } = body
+  const filter = {
+    email: user,
+    password
   }
-)
+  try {
+    const existUser = await login.getItem(filter, '-_id email')
+    console.log('jwt :>> ', jwt)
+    const token = await jwt.sign({ existUser }, 'waltertest')
+    console.log('token :>> ', token)
+    res.send({})
+  } catch (error) {
+    console.log('error :>> ', chalk.bgRedBright(error))
+    res.send('error')
+  }
+})
 
 module.exports = router
